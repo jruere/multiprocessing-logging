c3a6a602a16d7c7091fe9bf2cdbef707a1c37e08
Chris Withers <chris@simplistix.co.uk>
2018-01-29 18:43:42 +0000

    use mock on 2.7, so all tests always run

--- tests/test_multiprocessing_logging.py
+++ tests/test_multiprocessing_logging.py
@@ -14,7 +14,11 @@ from io import StringIO
 try:
     from unittest import mock
 except ImportError:
-    mock = None
+    try:
+        import mock
+    except ImportError:
+        mock = None
+
 
 from multiprocessing_logging import install_mp_handler, MultiProcessingHandler
 
@@ -32,9 +36,6 @@ class InstallHandlersTest(unittest.TestCase):
         self.assertIs(wrapper_handler.sub_handler, self.handler)
 
     def test_when_no_logger_is_specified_then_it_uses_the_root_logger(self):
-        if not mock:
-            self.skipTest('unittest.mock is not available')
-
         with mock.patch('logging.getLogger') as getLogger:
             getLogger.return_value = self.logger
 
@@ -47,9 +48,6 @@ class InstallHandlersTest(unittest.TestCase):
         self.assertIs(wrapper_handler.sub_handler, self.handler)
 
     def test_when_a_logger_is_passed_then_it_does_not_change_the_root_logger(self):
-        if not mock:
-            self.skipTest('unittest.mock is not available')
-
         with mock.patch('logging.getLogger') as getLogger:
             install_mp_handler(self.logger)
 
